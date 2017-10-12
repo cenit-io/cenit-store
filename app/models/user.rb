43bdaed40606186a04d0a2afca1936335f360c85
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2017-10-12 11:24:49 -0400

    Update
    Users roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
-  enum role: [:user, :admin, :silver, :gold, :platinum]
+  enum role: [:user, :admin]
   after_initialize :set_default_role, :if => :new_record?
   after_initialize :set_default_plan, :if => :new_record?
-  # after_create :sign_up_for_mailing_list
+  after_create :sign_up_for_mailing_list
 
   belongs_to :plan
   validates_associated :plan
@@ -26,12 +26,12 @@ class User < ActiveRecord::Base
 
   def subscribe
     mailchimp = Gibbon::Request.new(api_key: Rails.application.secrets.mailchimp_api_key)
-    list_id = Rails.application.secrets.mailchimp_list_id
-    result = mailchimp.lists(list_id).members.create(
-      body: {
-        email_address: self.email,
-        status: 'subscribed'
-    })
+    result = mailchimp.lists(Rails.application.secrets.mailchimp_list_id).members.create(
+        body: {
+            email_address: self.email,
+            status: 'subscribed'
+        }
+    )
     Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
   end
 
